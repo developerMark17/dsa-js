b86045b23e41b98fb0a0073a24444764b620f110
developerMark17 <webdevAiraz@gmail.com>
2026-03-03 12:52:54 +0530

    array-merge with built in methods

---
+++ array_merge.js
@@ -0,0 +1,27 @@
+// let arr = [2,5,6,7,9]
+// let arr2 = [20,40,60,80]
+// let arr3 = [];
+
+// for(let i =0; i<arr.length; i++){
+//     arr3[i] = arr[i]
+// }
+// for(let i =0; i<arr2.length; i++){
+//     arr3[arr.length+i] = arr2[i]    
+// }
+// console.log(arr3)
+
+
+let arr1 = [3,2,1,0];
+let arr2 = [1,2,3,4];
+let arr3 = []
+//built in methods
+console.log(arr1.reverse())
+console.log(...arr1,...arr2)
+// for(let i = 0; i <arr1.length; i++){
+//     arr3[i] = arr1[i]
+
+//     for(let i =0; i < arr2.length; i++){
+//         arr3[arr1.length+i] = arr2[i]
+//     }
+// }
+// console.log(arr3)
